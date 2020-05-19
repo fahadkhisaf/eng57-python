3b63b26d3d46d54dfc57c4d88e8dbed83e4cfd4e
fahadkhisaf <fkhisaf@gmail.com>
2020-05-19 15:37:29 +0100

    added excercises

---
+++ Python/Python_excercises/102_more_strings_variables.py
@@ -0,0 +1,25 @@
+# Practice strings
+# Welcome to Sparta - exercise
+
+# Version 1 specs - with concatenation
+# define a variable name, and assign a string
+
+# prompt the user for input and ask the user for his/her name
+# re assign the original variable this this input
+
+# use concatenation to print a welcome message and use methods to format the name/input (remove white spaces)
+
+# Version 2 - with interpolation
+
+# ask the user for a first name, save it in a variable
+
+# ask the user for a last name, save it in a variable
+
+# use interpolation to print the message
+
+
+# Calculate year of birth
+# gather details on age and name
+
+# print something like
+# OMG <person>, you are <age> years old so you were born in <year>
